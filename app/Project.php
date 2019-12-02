5f8afec319d2ca05e74298ee2d718f52bd9462c6
islam ahmed <islammashad@gmail.com>
2019-12-02 10:58:57 +0200

    define the relations between models

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [ 
+    	'project_name'
+    ] ;
+    public function tasks() {
+    	return $this->hasMany('App\Task');
+    }
+
 }
--- app/TaskFiles.php
+++ app/TaskFiles.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskFiles extends Model
 {
-    //
+    protected $fillable = ['task_id', 'filename'];
+    
+    public function task()
+    {
+        return $this->belongsTo('App\Task');
+    }
 }
